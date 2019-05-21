d5d61f9e6fa6efd8f7d8470c5de77d0dd35124ab
Everton Vinicius Prates <everton.prates@castinformatica.onmicrosoft.com>
2019-05-21 09:01:22 -0300

    Add project files.

---
+++ jointechbackend/contactController.js
@@ -0,0 +1,80 @@
+﻿// contactController.js
+// Import newsletter model
+Newsletter = require('./newsletterModel');
+// Handle index actions
+exports.index = function (req, res) {
+    Newsletter.get(function (err, newsletters) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+            });
+        }
+        res.json({
+            status: "success",
+            message: "Contacts retrieved successfully",
+            data: newsletters
+        });
+    });
+};
+// Handle create newsletter actions
+exports.new = function (req, res) {
+    var newsletter = new Newsletter();
+    newsletter.name = req.body.name ? req.body.name : newsletter.name;
+    newsletter.gender = req.body.gender;
+    newsletter.email = req.body.email;
+    newsletter.phone = req.body.phone;
+    // save the newsletter and check for errors
+    newsletter.save(function (err) {
+        // if (err)
+        //     res.json(err);
+        res.json({
+            message: 'New newsletter created!',
+            data: newsletter
+        });
+    });
+};
+// Handle view newsletter info
+exports.view = function (req, res) {
+    Newsletter.findById(req.params.contact_id, function (err, newsletter) {
+        if (err)
+            res.send(err);
+        res.json({
+            message: 'Newsletter details loading..',
+            data: newsletter
+        });
+    });
+};
+// Handle update newsletter info
+exports.update = function (req, res) {
+    Newsletter.findById(req.params.contact_id, function (err, newsletter) {
+        if (err)
+            res.send(err);
+        newsletter.name = req.body.name ? req.body.name : newsletter.name;
+        newsletter.gender = req.body.gender;
+        newsletter.email = req.body.email;
+        newsletter.phone = req.body.phone;
+        // save the newsletter and check for errors
+        newsletter.save(function (err) {
+            if (err)
+                res.json(err);
+            res.json({
+                message: 'Newsletter Info updated',
+                data: newsletter
+            });
+        });
+    });
+};
+// Handle delete newsletter
+exports.delete = function (req, res) {
+    Newsletter.remove({
+        _id: req.params.contact_id
+    }, function (err, newsletter) {
+        if (err)
+            res.send(err);
+        res.json({
+            status: "success",
+            message: 'Newsletter deleted'
+        });
+    });
+};
---
+++ jointechbackend/newsletterModel.js
@@ -0,0 +1,14 @@
+﻿var mongoose = require('mongoose');
+// Setup schema
+var newsletter = mongoose.Schema({
+    _id: String,
+    cardTitle: String,
+    img: String,
+    description: String,
+    lastupdate: String
+});
+// Export Contact model
+var Newsletter = module.exports = mongoose.model('newsletter', newsletter);
+module.exports.get = function (callback, limit) {
+    Newsletter.find(callback).limit(limit);
+}
